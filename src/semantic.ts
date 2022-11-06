0b22c56ace9edd1181dd3ab23f175b2bc46941d4
Jauhar Arifin <jauhararifin10@gmail.com>
2022-11-06 19:21:30 +0700

    bootstrap semantic representation of the language

--- src/semantic.ts
+++ src/semantic.ts
@@ -41,10 +41,19 @@ export interface ArrayType {
 }
 
 export type Statement = BlockStatement
+| IfStatement
+| WhileStatement
+| AssignStatement
+| ExprStatement
+| ReturnStatement
 
 export enum StatementKind {
   Block,
   If,
+  While,
+  Assign,
+  Expr,
+  Return,
 }
 
 export interface BlockStatement {
@@ -59,4 +68,105 @@ export interface IfStatement {
   else?: Statement
 }
 
-export type Expr = null
+export interface WhileStatement {
+  kind: StatementKind.While
+  condition: Expr
+  body: Statement
+}
+
+export interface AssignStatement {
+  kind: StatementKind.Assign
+  target: Expr
+  value: Expr
+}
+
+export interface ExprStatement {
+  kind: StatementKind.Expr
+  value: Expr
+}
+
+export interface ReturnStatement {
+  kind: StatementKind.Return
+  value?: Expr
+}
+
+export type Expr = BinaryExpr
+| UnaryExpr
+| IndexExpr
+| CastExpr
+| CallExpr
+| IntegerLitExpr
+| CharLitExpr
+| BooleanLitExpr
+| IdentExpr
+
+export enum ExprKind {
+  Binary,
+  Unary,
+  Index,
+  Cast,
+  Call,
+  IntegerLit,
+  CharLit,
+  BooleanLit,
+  Ident,
+}
+
+export interface BinaryExpr {
+  kind: ExprKind.Binary
+  a: Expr
+  op: BinaryOp
+  b: Expr
+}
+
+export enum BinaryOp {
+  Plus, Minus, Div, Mul, And, Or, BitAnd, BitOr, BitXor
+}
+
+export interface UnaryExpr {
+  kind: ExprKind.Unary
+  op: UnaryOp
+  value: Expr
+}
+
+export enum UnaryOp {
+  Plus, Minus, Not, BitNot
+}
+
+export interface IndexExpr {
+  kind: ExprKind.Index
+  array: Expr
+  index: Expr
+}
+
+export interface CastExpr {
+  kind: ExprKind.Cast
+  source: Expr
+  type: Type
+}
+
+export interface CallExpr {
+  kind: ExprKind.Call
+  function: Expr
+  arguments: Expr[]
+}
+
+export interface IntegerLitExpr {
+  kind: ExprKind.IntegerLit
+  value: BigInt
+}
+
+export interface CharLitExpr {
+  kind: ExprKind.CharLit
+  value: string
+}
+
+export interface BooleanLitExpr {
+  kind: ExprKind.BooleanLit
+  value: boolean
+}
+
+export interface IdentExpr {
+  kind: ExprKind.Ident
+  ident: string
+}
